45b79e7a12969562a3f179552239c65ede5867c7
hhhh0na <0903.ldh@gmail.com>
2023-04-07 11:19:26 +0900

    Update samples.json, develpment.sh

--- people_count_back/develpment.sh
+++ people_count_back/develpment.sh
@@ -1,16 +1,16 @@
-pip install -r requirements.txt -t /home/ec2-user/environment/people_count_back/lambda
+pip install -r requirements.txt -t /home/ec2-user/environment/people-counter-for-events/people_count_back/lambda
 
 REGION=$(aws configure get region)
 UUID=$(uuidgen | awk '{print tolower($0)}')
 S3_BUCKET=samples-bucket-$UUID
 aws s3 mb "s3://${S3_BUCKET}" --region $REGION
 
-cd /home/ec2-user/environment/people_count_back/lambda
+cd /home/ec2-user/environment/people-counter-for-events/people_count_back/lambda
 
 #Upload zip lambda file to S3bucket 
 zip -r ../index * 
 
-cd /home/ec2-user/environment/people_count_back/
+cd /home/ec2-user/environment/people-counter-for-events/people_count_back/
 
 aws s3 cp index.zip "s3://${S3_BUCKET}/"
 
@@ -19,11 +19,11 @@ aws s3 cp index.zip "s3://${S3_BUCKET}/"
 
 aws cloudformation create-stack \
   --stack-name people-count-stack \
-  --template-body file:///home/ec2-user/environment/people_count_back/samples.json \
+  --template-body file:///home/ec2-user/environment/people-counter-for-events/people_count_back/samples.json \
   --parameters ParameterKey=BucketName,ParameterValue=${S3_BUCKET} \
   --capabilities CAPABILITY_NAMED_IAM 
 
 ##deploy frontend
 
-cd /home/ec2-user/environment/people_count_front
+cd /home/ec2-user/environment/people-counter-for-events/people_count_front
 npm install
